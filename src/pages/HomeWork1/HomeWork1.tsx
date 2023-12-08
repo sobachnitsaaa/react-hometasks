86ae17c7fcd0957a5eff482dc2956e4ec03d69f9
Shevchenko Ekaterina <ekaterina.shevchenko@ax-team.corp>
2023-12-08 11:08:39 +0300

    homework1

--- src/pages/HomeWork1/HomeWork1.tsx
+++ src/pages/HomeWork1/HomeWork1.tsx
@@ -1,3 +1,47 @@
+import styles from './styles.module.css';
+import imgOne from '../../assets/img/one.png';
+import imgTwo from '../../assets/img/two.png';
+import imgThree from '../../assets/img/three.png';
+import imgFour from '../../assets/img/four.png';
+import imgFive from '../../assets/img/five.png';
+import imgSix from '../../assets/img/six.png';
+import {useState} from "react";
+import classNames from "classnames";
+
+const SIDES = {
+    1: imgOne,
+    2: imgTwo,
+    3: imgThree,
+    4: imgFour,
+    5: imgFive,
+    6: imgSix,
+};
+
 export const HomeWork1 = () => {
-  return <div>Решение домашнего задания №1</div>;
+    const[twist, setTwist] = useState(1);
+    const[animation, setAnimation] = useState(false);
+
+    const handleClick = () => setTwist(Math.floor(Math.random() * 6) + 1);
+
+    console.log(animation);
+
+  return (
+      <>
+        <div>Решение домашнего задания №1</div>
+
+        <div className={styles.container}>
+            <button className={styles.btn} onClick={() => {
+                handleClick();
+                setAnimation(true)
+            }}>
+                Нажми на меня
+            </button>
+            <img
+                src={SIDES[twist as keyof typeof SIDES]}
+                alt='imgOne'
+                className={classNames(styles.cube, animation && styles.animation)}
+                onAnimationEnd={() => setAnimation(false)}/>
+        </div>
+      </>
+  )
 };
